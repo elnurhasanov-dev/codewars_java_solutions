27b856544d24286d73f76dbd330f2726317778a9
thehasanovv <hasanov.e95@gmail.com>
2024-06-27 21:10:14 +0400

    Added solution

---
+++ src/kyu7/SumOfOddNumbers.java
@@ -0,0 +1,11 @@
+package kyu7;
+
+public class SumOfOddNumbers {
+    public static void main(String[] args) {
+    }
+
+    // Solution
+    public static int rowSumOddNumbers(int n) {
+        return (int) Math.pow(n, 3);
+    }
+}
