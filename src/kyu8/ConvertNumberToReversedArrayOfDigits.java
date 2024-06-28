831cb67c8af7a4117e6075cc89286bc51d13410c
thehasanovv <hasanov.e95@gmail.com>
2024-06-28 15:27:59 +0400

    Added solution

--- src/kyu8/ConvertNumberToReversedArrayOfDigits.java
+++ src/kyu8/ConvertNumberToReversedArrayOfDigits.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 public class ConvertNumberToReversedArrayOfDigits {
     public static void main(String[] args) {
         int[] test1 = digitize(654321);
-        int[] test2 = digitize(123456789);
+        int[] test2 = digitize2(123456789);
 
         System.out.println(Arrays.toString(test1));
         System.out.println(Arrays.toString(test2));
 
     }
-
+    // Solution 1
     private static int[] digitize(long n) {
         String temp = Long.toString(n);
         int[] newGuess = new int[temp.length()];
@@ -22,5 +22,14 @@ public class ConvertNumberToReversedArrayOfDigits {
         }
 
         return newGuess;
+    }
+
+    // Solution 2
+    private static int[] digitize2(long n) {
+        return new StringBuilder(String.valueOf(n))
+                .reverse()
+                .chars()
+                .map(Character::getNumericValue)
+                .toArray();
     }
 }
