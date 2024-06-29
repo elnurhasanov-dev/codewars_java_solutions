00f83a3313ada6a9a3045ee63bc765f459184291
thehasanovv <hasanov.e95@gmail.com>
2024-06-29 16:22:41 +0400

    Added solution

---
+++ src/kyu8/SumArrays.java
@@ -0,0 +1,27 @@
+package kyu8;
+
+import java.util.Arrays;
+
+public class SumArrays {
+    public static void main(String[] args) {
+        double test1 = sum(new double[]{1, 5.2, 4, 0, -1});
+        double test2 = sum2(new double[]{2, 2, 1, 2, 1, 3});
+
+        System.out.println(test1);
+        System.out.println(test2);
+    }
+
+    // Solution 1
+    public static double sum(double[] numbers) {
+        return Arrays.stream(numbers).sum();
+    }
+
+    // Solution 2
+    public static double sum2(double[] numbers) {
+        double result = 0;
+
+        for (double value : numbers) result += value;
+
+        return result;
+    }
+}
