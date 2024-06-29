915405173c577c352bbf81d616e6a5581ccfa1f4
thehasanovv <hasanov.e95@gmail.com>
2024-06-29 15:34:03 +0400

    Added solution

---
+++ src/kyu7/DontGiveMeFive.java
@@ -0,0 +1,30 @@
+package kyu7;
+
+import java.util.stream.IntStream;
+
+public class DontGiveMeFive {
+    public static void main(String[] args) {
+        int test1 = dontGiveMeFive(1, 9);
+        int test2 = dontGiveMeFive(4, 17);
+
+        System.out.println(test1);
+        System.out.println(test2);
+    }
+
+    // Solution 1
+    public static int dontGiveMeFive(int start, int end) {
+        return (int) IntStream
+                .range(start, end + 1)
+                .filter(x -> !String.valueOf(x).contains("5"))
+                .count();
+    }
+
+    // Solution 2
+    public static int dontGiveMeFive2(int start, int end) {
+        int ans = 0;
+        for (int i = start; i <= end; i++) {
+            if (!("" + i).contains("5")) ans++;
+        }
+        return ans;
+    }
+}
